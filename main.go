a0cc0374d91a81bfea1c5db1bf082c51bfe688b3
Michael P. Soulier <msoulier@digitaltorque.ca>
2025-08-12 10:55:19 -0400

    Adding a string input test mode

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ package main
 // when all done, translate multiple, adjacent underscores to a single one
 import (
 	"path/filepath"
+	"io"
 	"io/fs"
 	"os"
 	"strings"
@@ -24,12 +25,16 @@ var (
 	log *logging.Logger = nil
 	debug = false
 	confirm = false
+	directories = false
+	string_input = false
 	args []string
 )
 
 func init() {
 	flag.BoolVar(&debug, "d", false, "Debug logging")
 	flag.BoolVar(&confirm, "c", false, "Confirm all moves")
+	flag.BoolVar(&directories, "D", false, "Rename directories too")
+	flag.BoolVar(&string_input, "s", false, "Apply clean algorithm to stdin")
 	flag.Parse()
 	args = flag.Args()
 
@@ -47,18 +52,24 @@ func init() {
 	}
 	log = logging.MustGetLogger("sf")
 
-	if len(args) < 1 {
+	if len(args) < 1 && ! string_input {
 		log.Errorf("Usage: %s <path/files>", os.Args[0])
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
 }
 
-func clean_name(path string) (string, bool) {
+func clean_name(path string, simple bool) (string, bool) {
 	log.Debugf("clean_name: path is %s", path)
 	changed := false
-	base := filepath.Base(path)
-	dir := filepath.Dir(path)
+	var base, dir string
+	if simple {
+		base = path
+		dir = ""
+	} else {
+		base = filepath.Base(path)
+		dir = filepath.Dir(path)
+	}
 	log.Debugf("basename is %s", base)
 	origname := base
 
@@ -76,6 +87,13 @@ func clean_name(path string) (string, bool) {
 	// when all done, translate multiple, adjacent underscores to a single one
 	mult_underscores_pat := regexp.MustCompile("_+")
 	base = mult_underscores_pat.ReplaceAllLiteralString(base, "_")
+	// Remove any leading or trailing underscores.
+	if base[0] == '_' {
+		base = base[1:]
+	}
+	if base[len(base)-1] == '_' {
+		base = base[:len(base)-1]
+	}
 
 	if strings.Compare(origname, base) == 0 {
 		changed = false
@@ -83,15 +101,38 @@ func clean_name(path string) (string, bool) {
 		changed = true
 	}
 
-	return filepath.Join(dir, base), changed
+	log.Debugf("base is now '%s', changed is %v", base, changed)
+	if simple {
+		return base, changed
+	} else {
+		return filepath.Join(dir, base), changed
+	}
 }
 
 func main() {
 	stdin_reader := bufio.NewReader(os.Stdin)
 	stdout_writer := bufio.NewWriter(os.Stdout)
 
+	if string_input {
+		// Read from stdin, clean and print to stdout.
+		for {
+			line, err := stdin_reader.ReadString('\n')
+			if err != nil {
+				if err == io.EOF {
+					os.Exit(0)
+				} else {
+					panic(err)
+				}
+			}
+			newline, _ := clean_name(line, true)
+			stdout_writer.WriteString(newline + "\n")
+			stdout_writer.Flush()
+		}
+	}
+
 	for _, path := range args {
 		log.Debugf("%s", path)
+		rename_directories := make([]string, 0)
 		filepath.Walk(path, func(fpath string, info fs.FileInfo, err error) error {
 			if err != nil {
 				log.Errorf("Walk error: %s", err)
@@ -100,9 +141,14 @@ func main() {
 			log.Debugf("fpath is %s", fpath)
 			if info.IsDir() {
 				log.Debugf("%s is a directory", fpath)
-				// FIXME switches to control directory renaming behaviour
+				if directories {
+					_, changed := clean_name(fpath, false)
+					if changed {
+						rename_directories = append(rename_directories, fpath)
+					}
+				}
 			} else {
-				newname, changed := clean_name(fpath)
+				newname, changed := clean_name(fpath, false)
 				log.Debugf("newname is %s", newname)
 				if changed {
 					log.Debugf("===> filename has changed, need to rename")
@@ -134,5 +180,16 @@ func main() {
 			}
 			return nil
 		})
+		if len(rename_directories) > 0 {
+			log.Debugf("There are directories to rename")
+			for _, dir := range rename_directories {
+				newname, changed := clean_name(dir, false)
+				if changed {
+					log.Debugf("should change %s to %s", dir, newname)
+				} else {
+					panic("we should not be here")
+				}
+			}
+		}
 	}
 }
